a333bb1634b0a6497e326c51e0bf22041cf9cb4c
Chris Prater <cprater23@gmail.com>
2014-02-19 12:29:42 -0600

    donezoe

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -13,16 +13,14 @@ get '/:category_id' do
   erb :list_posts
 end
 
-get '/:category_id/new' do
-  
+get '/:category_id/new' do  
   @id = params[:category_id]
-  # binding.pry
   @category = Category.find(params[:category_id])
   erb :create_post
 end
 
-get '/post/edit' do
-  @post = Post.find(@post_id)
+get '/:post_id/edit' do
+  @post = Post.find(params[:post_id])
   erb :edit_post
 end
 
@@ -30,11 +28,6 @@ get '/:category_id/:post_id' do
   @post = Post.find(params[:post_id])
   erb :post
 end
-
-# get '/:category_id/:post_id/edit' do
-#   @post = Post.find(params[:post_id]) 
-#   erb :edit_post
-# end
 
 get '/categories/new' do
 end
@@ -50,25 +43,8 @@ post '/new_category' do
 end
 
 post '/post/edit' do 
-  Post.update(content: params[:content])
+  @post = Post.find(params[:post_id])
+  @post.update_attributes(content: params[:content])
+  erb :post
 end
-
-
-
-
-# get '/categories/:category_id/posts/new' do
-#   @category = Category.find(params[:category_id])
-#   erb :new_post
-# end
-
-# post '/categories/:category_id/posts' do
-#   puts params.inspect
-#   @category = Category.find(params[:category_id])
-#   @category.posts << Post.new(params[:post])
-#   # redirect to categories/id page
-#   200
-# end
-
-# post '/categories' do
-# end
 
